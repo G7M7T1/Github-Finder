5955836b412aa9326b23084417e5cb01aeac9079
G7M7T1 <genlisense@gmail.com>
2022-03-12 19:35:55 -0500

    env

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,23 +1,22 @@
 const About = () => {
     return(
         <>
-            <h1 className='text-6xl mb-4'>Github Finder</h1>
+            <h1 className='text-6xl mb-4 font-black'>Github Finder</h1>
             <p className='mb-4 text-2xl font-light'>
                 A React app to search GitHub profiles and see profile details. This app are Using <strong>Tailwind</strong> and <strong>Daisy UI</strong> and <strong>React Icons</strong>
             </p>
 
             <p className='mb-4 text-2xl font-light'>
-                <strong>Daisy UI</strong>: <a target="_blank" href="https://daisyui.com/">https://daisyui.com/</a>
+                <strong>Daisy UI</strong>: <a rel="noreferrer" target="_blank" href="https://daisyui.com/">https://daisyui.com/</a>
             </p>
 
             <p className='mb-4 text-2xl font-light'>
-                <strong>Tailwind</strong>: <a target="_blank" href="https://tailwindcss.com/">https://tailwindcss.com/</a>
+                <strong>Tailwind</strong>: <a rel="noreferrer" target="_blank" href="https://tailwindcss.com/">https://tailwindcss.com/</a>
             </p>
 
             <p className='mb-4 text-2xl font-light'>
-                <strong>React Icons</strong>: <a target="_blank" href="https://react-icons.github.io/react-icons/">https://react-icons.github.io/react-icons/</a>
+                <strong>React Icons</strong>: <a rel="noreferrer" target="_blank" href="https://react-icons.github.io/react-icons/">https://react-icons.github.io/react-icons/</a>
             </p>
-
         </>
     )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,8 @@ const Home = () => {
     return(
         <div>
             <h1 className="text-6xl">Welcome</h1>
+            {process.env.REACT_APP_GITHUB_TOKEN}
+            {process.env.REACT_APP_GITHUB_URL}
         </div>
     )
 }
