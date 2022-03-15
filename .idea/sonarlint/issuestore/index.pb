8f344d5410c6a26c0e9f9be103e7003add58d434
G7M7T1 <genlisense@gmail.com>
2022-03-15 09:02:29 -0400

    Add Reducer

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -3,9 +3,6 @@ A
 public/index.html,f\4\f43ecdf8d666ee908aacce037b4cd72159e4741b
 A
 postcss.config.js,3\f\3f70684581fd16ab3be77ca959e0138ef3ee8bfb
-:
-
-src/App.js,1\e\1e43fa63aecd2127be034b3fb9d4caa172275714
 <
 package.json,7\0\7030d0b2f71b999ff89a343de08c414af32fc93a
 C
@@ -14,5 +11,3 @@ B
 tailwind.config.js,0\9\0918cbb76f2c44f223706cec9d31a32cd1177ce4
 F
 src/pages/NotFound.jsx,0\f\0fd26c0d3021f12cc22c9ac0036c0f3caab65157
-4
-.env,3\c\3c84dcdc6bbe3d7817c49dcdc327b926fea1808a
--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,26 +1,14 @@
-import {useEffect, useState} from "react";
+import {useEffect, useContext} from "react";
 import Spinner from "../layout/Spinner";
 import UserItem from "../users/UserItem"
+import GithubContext from "../../context/github/GithubContext";
 
 const UserResults = () => {
-    const [users, setUsers] = useState([])
-    const [Loading, setLoading] = useState(true)
+    const {users, Loading, fetchUsers} = useContext(GithubContext)
 
     useEffect(() => {
         fetchUsers()
     },[])
-
-    const fetchUsers = async () => {
-        const response = await fetch(`${process.env.REACT_APP_GITHUB_URL}users`, {
-            headers: {
-                Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`
-            }
-        })
-
-        const data = await response.json()
-        setUsers(data)
-        setLoading(false)
-    }
 
     if(!Loading) {
         return(
